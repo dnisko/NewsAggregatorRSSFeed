3c59552cc7be8eb92d82f36ece331a6c0330aa64
dnisko <dnisko@hotmail.com>
2024-06-30 00:42:23 +0200

    added fixed port

--- NewsAggregatorRSSFeed/Program.cs
+++ NewsAggregatorRSSFeed/Program.cs
@@ -6,12 +6,46 @@ namespace NewsAggregatorRSSFeed
     {
         public static void Main(string[] args)
         {
+
             var builder = WebApplication.CreateBuilder(args);
 
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowSpecificOrigin",
+                    builder =>
+                    {
+                        builder.AllowAnyOrigin()
+                            .AllowAnyMethod()
+                            .AllowAnyHeader();
+                    });
+            });
+
             // Register ApiService and RssService in the dependency injection container
-            builder.Services.AddSingleton<ApiService>();
+            builder.Services.AddTransient<ApiService>();
 
             builder.Services.AddControllersWithViews();
+
+            //https://stackoverflow.com/a/55276201
+            //https://learn.microsoft.com/en-us/aspnet/core/fundamentals/servers/kestrel/endpoints?view=aspnetcore-6.0
+            //+ various links
+            var config = builder.Configuration;
+            builder.WebHost.ConfigureKestrel(options =>
+            {
+                var httpUrl = config["Kestrel:Endpoints:Http:Url"];
+                if (!string.IsNullOrEmpty(httpUrl))
+                {
+                    options.ListenAnyIP(new Uri(httpUrl).Port); // Listen on all IP addresses
+                }
+
+                var httpsUrl = config["Kestrel:Endpoints:Https:Url"];
+                if (!string.IsNullOrEmpty(httpsUrl))
+                {
+                    options.ListenAnyIP(new Uri(httpsUrl).Port, listenOptions =>
+                    {
+                        listenOptions.UseHttps(); // Enable HTTPS
+                    });
+                }
+            });
 
             var app = builder.Build();
 
@@ -26,6 +60,9 @@ namespace NewsAggregatorRSSFeed
             app.UseStaticFiles();
 
             app.UseRouting();
+
+            // Apply CORS policy globally
+            app.UseCors("AllowSpecificOrigin");
 
             app.UseAuthorization();
 
